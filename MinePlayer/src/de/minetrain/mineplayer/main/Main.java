77d9651be0025484925faa10981db9865a5bd647
MineTrainDevelopment <minetrain.dev@gmail.com>
2024-09-09 14:29:31 +0200

    Initial commit.

---
+++ MinePlayer/src/de/minetrain/mineplayer/main/Main.java
@@ -0,0 +1,17 @@
+package de.minetrain.mineplayer.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+	private static final Logger logger = LoggerFactory.getLogger(Main.class);
+	
+	/**
+	 * This seams to be the start of the End.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		logger.info("Hello World :D");
+	}
+	
+}
